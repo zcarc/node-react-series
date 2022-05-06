a7697fc37b949d6df7e304f859b313f218e01494
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-06 23:30:41 +0900

    Add Auth 기능 만들기 - 기본 강의

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const { User } = require("./models/User");
-
 const mongoose = require("mongoose");
+const { auth } = require("./middleware/auth");
 
 mongoose
   .connect(config.mongoURI)
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
   res.send("Hello World! 안녕하세요!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원가입 할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body);
@@ -43,7 +43,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -70,6 +70,20 @@ app.post("/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 것을 의미한다.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
