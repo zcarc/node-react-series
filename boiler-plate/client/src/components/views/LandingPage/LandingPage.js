c6f31d778c2e4afe24ac55a241ba0e6a43a4add7
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-07 16:19:57 +0900

    Add setupProxy.js

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -4,9 +4,9 @@ import axios from "axios";
 function LandingPage() {
   useEffect(() => {
     // 클라이언트와 서버의 포트가 달라서 CORS 에러가 발생한다.
-    axios
-      .get("http://localhost:3010/api/hello")
-      .then((res) => console.log(res.data));
+    // setupProxy.js를 작성하면 문제를 해결할 수 있다.
+    axios.get("/api/hello").then((res) => console.log(res));
+    console.log("landing");
   }, []);
 
   return <div>LandingPage</div>;
