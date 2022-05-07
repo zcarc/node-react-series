e5a6209a9395e60aa4fcfe2cbda14ae5a4d2b2b4
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-07 11:18:28 +0900

    Add cra, react-router-dom

---
+++ boiler-plate/client/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
