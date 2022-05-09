1300dd71746d3594b5ec907a859d9c0d8fa2c587
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-09 22:56:29 +0900

    Add 로그아웃

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     // 클라이언트와 서버의 포트가 달라서 CORS 에러가 발생한다.
     // setupProxy.js를 작성하면 문제를 해결할 수 있다.
@@ -9,7 +12,30 @@ function LandingPage() {
     console.log("landing");
   }, []);
 
-  return <div>LandingPage</div>;
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃 하는데 실패 했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
 }
 
 export default LandingPage;
