067ac581214f6afb749714478cfe6e047e2fe8a4
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-11 17:36:25 +0900

    Add Load more button for pagination

--- boiler-plate-movie/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate-movie/client/src/components/views/LandingPage/LandingPage.js
@@ -8,17 +8,30 @@ import MainImage from "./Sections/MainImage";
 function LandingPage() {
   const [Movies, setMovies] = useState([]);
   const [MainMovieImage, setMainMovieImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0);
 
-  useEffect(() => {
-    const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
+  const fetchMovies = (endpoint) => {
+    console.log("fetchMovies... endpoint: ", endpoint);
     fetch(endpoint)
       .then((response) => response.json())
       .then((response) => {
-        console.log(response.results[0]);
-        setMovies(response.results);
+        console.log("response.results: ", response.results);
+        setMovies([...Movies, ...response.results]);
         setMainMovieImage(response.results[0]);
+        setCurrentPage(response.page);
       });
+  };
+
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${
+      CurrentPage + 1
+    }`;
+    fetchMovies(endpoint);
+  };
+
+  useEffect(() => {
+    const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint);
   }, []);
 
   return (
@@ -59,7 +72,7 @@ function LandingPage() {
       </div>
 
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <button> Load More</button>
+        <button onClick={loadMoreItems}> Load More</button>
       </div>
     </div>
   );
--- boiler-plate-movie/client/src/components/views/LandingPage/Sections/MainImage.js
+++ boiler-plate-movie/client/src/components/views/LandingPage/Sections/MainImage.js
@@ -4,16 +4,15 @@ function MainImage(props) {
   return (
     <div
       style={{
-        background: `linear-gradient(to bottom, rgba(0,0,0,0)
-        39%,rgba(0,0,0,0)
-        41%,rgba(0,0,0,0.65)
-        100%),
-        url('${props.image}'), #1c1c1c`,
+        position: "relative",
+        width: "100%",
         height: "500px",
+        background: `linear-gradient(to bottom, rgba(0,0,0,0) 39%, 
+        rgba(0,0,0,0) 41%, rgba(0,0,0,0.65) 100%),
+        `,
+        backgroundImage: `url("${props.image}")`,
         backgroundSize: "100%, cover",
         backgroundPosition: "center, center",
-        width: "100%",
-        position: "relative",
       }}
     >
       <div>
