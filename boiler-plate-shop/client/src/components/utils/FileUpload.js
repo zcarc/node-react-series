20675f82566cec6bbb539d64fbf9bbcf8646ca6a
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-14 23:09:16 +0900

    Add multer를 사용해서 서버에 이미지 저장, 저장한 이미지 렌더링

--- boiler-plate-shop/client/src/components/utils/FileUpload.js
+++ boiler-plate-shop/client/src/components/utils/FileUpload.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
 import axios from "axios";
 
 function FileUpload() {
+  const [Images, setImages] = useState([]);
+
   const dropHandler = (files) => {
     let formData = new FormData();
     const config = {
@@ -13,7 +15,10 @@ function FileUpload() {
 
     axios.post("/api/product/image", formData, config).then((response) => {
       if (response.data.success) {
+        console.log("response.data.filePath: ", response.data.filePath);
+        setImages([...Images, response.data.filePath]);
       } else {
+        console.log(response);
         alert("파일을 저장하는데 실패했습니다.");
       }
     });
@@ -39,6 +44,24 @@ function FileUpload() {
           </div>
         )}
       </Dropzone>
+
+      <div
+        style={{
+          display: "flex",
+          width: "350px",
+          height: "240px",
+          overflowX: "scroll",
+        }}
+      >
+        {Images.map((image, index) => (
+          <div key={index}>
+            <img
+              style={{ minWidth: "300px", width: "300px", height: "240px" }}
+              src={`http://localhost:3010/${image}`}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- boiler-plate-shop/server/routes/product.js
+++ boiler-plate-shop/server/routes/product.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const fs = require("fs");
 
 //=================================
 //             Product
 //=================================
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const path = "./uploads";
+    fs.mkdirSync(path, { recursive: true });
+    cb(null, path);
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
 // 여행 상품 업로드에서 드랍존에 파일 드랍 시 파일을 저장하는 API
-router.post("/image", (req, res) => {});
+router.post("/image", (req, res) => {
+  // 가져온 이미지 저장
+
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      filePath: req.file.path,
+      fileName: req.file.filename,
+    });
+  });
+});
 
 module.exports = router;
