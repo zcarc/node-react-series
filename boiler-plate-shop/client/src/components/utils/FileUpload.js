7d3cfca9b431a670a77943359e3110eaf2ef3b1a
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-14 20:50:54 +0900

    Add Dropzone Change 함수, API, multer

--- boiler-plate-shop/client/src/components/utils/FileUpload.js
+++ boiler-plate-shop/client/src/components/utils/FileUpload.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
+import axios from "axios";
 
 function FileUpload() {
+  const dropHandler = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/fomr-data" },
+    };
+    formData.append("file", files[0]);
+
+    axios.post("/api/product/image", formData, config).then((response) => {
+      if (response.data.success) {
+      } else {
+        alert("파일을 저장하는데 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+      <Dropzone onDrop={dropHandler}>
         {({ getRootProps, getInputProps }) => (
           <div
             style={{
---
+++ boiler-plate-shop/server/routes/product.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+// 여행 상품 업로드에서 드랍존에 파일 드랍 시 파일을 저장하는 API
+router.post("/image", (req, res) => {});
+
+module.exports = router;
