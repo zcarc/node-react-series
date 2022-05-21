009c0fad8f48c6dc544483704475b7d55add2678
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-21 15:54:42 +0900

    Add Cart 페이지, 모든 상품 가격 합계 렌더링

--- boiler-plate-shop/client/src/components/views/CartPage/CartPage.js
+++ boiler-plate-shop/client/src/components/views/CartPage/CartPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_actions";
 import UserCardBlock from "./Sections/UserCardBlock";
@@ -6,6 +6,8 @@ import UserCardBlock from "./Sections/UserCardBlock";
 function CartPage(props) {
   console.log("CartPage... props: ", props);
   const dispatch = useDispatch();
+
+  const [Total, setTotal] = useState(0);
 
   useEffect(() => {
     let cartItems = [];
@@ -16,16 +18,34 @@ function CartPage(props) {
           cartItems.push(item.id);
         });
 
-        dispatch(getCartItems(cartItems, props.user.userData.cart));
+        dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
+          (response) => {
+            calculateTotal(response.payload);
+          }
+        );
       }
     }
   }, [props.user.userData]);
+
+  let calculateTotal = (cartDetail) => {
+    let total = 0;
+
+    cartDetail.map((item) => {
+      total += item.price * item.quantity;
+    });
+
+    setTotal(total);
+  };
 
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
       <h1>My Cart</h1>
       <div>
         <UserCardBlock products={props.user.cartDetail} />
+      </div>
+
+      <div style={{ marginTop: "3rem" }}>
+        <h2>Total Amount: ${Total}</h2>
       </div>
     </div>
   );
