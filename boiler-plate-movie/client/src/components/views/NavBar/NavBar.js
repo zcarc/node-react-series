97c58e33f6e4824dfe014e0d2755299f178b0430
Hyunsoo Lee <wrkbrs@gmail.com>
2022-05-23 00:02:08 +0900

    Update Navbar 주석제거 - movie

--- boiler-plate-movie/client/src/components/views/NavBar/NavBar.js
+++ boiler-plate-movie/client/src/components/views/NavBar/NavBar.js
@@ -17,23 +17,6 @@ function NavBar() {
   };
 
   return (
-    // <nav
-    //   className="menu"
-    //   style={{ position: "fixed", zIndex: 5, width: "100%" }}
-    // >
-    //   <div className="menu__logo">
-    //     <a href="/">Logo</a>
-    //   </div>
-    //   <div className="menu__container">
-    //     <div>
-    //       <LeftMenu mode="horizontal" />
-    //     </div>
-    //     <div>
-    //       <RightMenu mode="horizontal" />
-    //     </div>
-    //   </div>
-    // </nav>
-
     <nav
       className="menu"
       style={{ position: "fixed", zIndex: 5, width: "100%" }}
